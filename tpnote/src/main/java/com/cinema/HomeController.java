f1f63953f9907cb2a42a4d552b68520b0d03b93b
kahinaMez <kahina.meziani@hotmail.com>
2020-03-25 20:09:49 +0100

    Teasting first interface

---
+++ tpnote/src/main/java/com/cinema/HomeController.java
@@ -0,0 +1,20 @@
+package com.cinema;
+
+import java.io.Serializable;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeController implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@FXML
+	Button btn1;
+	
+	public void sayHello() {
+		System.out.println("Hello world");
+	}
+}
