d37cbb32b6e7634283dbdc2c286aaeaf8128f261
kahinaMez <kahina.meziani@hotmail.com>
2020-03-25 20:56:22 +0100

    Creation ProjectionDTO

---
+++ tpnote/src/main/java/com/cinema/dto/ProjectionDto.java
@@ -0,0 +1,60 @@
+package com.cinema.dto;
+
+import java.sql.Date;
+
+public class ProjectionDto {
+	private int id;
+	private Date date;
+	private int nb_place_libre;
+	private float tarif;
+	
+	public ProjectionDto() {
+		super();
+	}
+	
+	public ProjectionDto(int id, Date date, int nb_place_libre, float tarif) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.nb_place_libre = nb_place_libre;
+		this.tarif = tarif;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getNb_place_libre() {
+		return nb_place_libre;
+	}
+
+	public void setNb_place_libre(int nb_place_libre) {
+		this.nb_place_libre = nb_place_libre;
+	}
+
+	public float getTarif() {
+		return tarif;
+	}
+
+	public void setTarif(float tarif) {
+		this.tarif = tarif;
+	}
+
+	
+	
+	
+	
+
+}
